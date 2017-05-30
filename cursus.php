6d7100919df5a0673f633ff560843e65a2103635
Lyes <Lyes@10.18.12.136>
2017-05-30 15:20:02 +0200

    nav bar maj

--- cursus.php
+++ cursus.php
@@ -1,9 +1,17 @@
 <html>
     <head>
         <meta charset="UTF-8">
+          <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
         <title></title>
     </head>
     <body>
+        <ul class="nav nav-pills col-lg-8 col-lg-offset-2">
+            <li role="presentation"><a href="index.php">Accueil</a></li>
+  <li role="presentation"><a href="etu.php">Etudiant</a></li>
+  <li role="presentation" class="active"><a href="cursus.php">Cursus</a></li>
+  <li role="presentation"><a href="reglement.php">Réglement</a></li>
+        </ul>
+        
         <h1>Cursus</h1>
         
         <h4>Ajouter un cursus</h4>
--- index.php
+++ index.php
@@ -7,12 +7,17 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
         <title></title>
     </head>
     <body>
-        <h3>Etudiant</h3>
-        <h3>Cursus</h3>
-        <h3>Etudiant</h3>
+        <ul class="nav nav-pills col-lg-8 col-lg-offset-2">
+            <li role="presentation" class="active"><a href="index.php">Accueil</a></li>
+  <li role="presentation"><a href="etu.php">Etudiant</a></li>
+  <li role="presentation"><a href="cursus.php">Cursus</a></li>
+  <li role="presentation"><a href="reglement.php">Réglement</a></li>
+        </ul>
+        <h1>Bienvenue</h1>
         
     </body>
 </html>
